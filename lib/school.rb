21725f30e594016e0dce04a7c0e0d8a03c9b6cd9
Mollie Dickenson <molliedickenson@gmail.com>
2021-05-19 01:17:34 -0400

    commit tree 3b54c585b8d6b0b738c2f0c694fa0cef8aeeb7f0

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+
+class School
+    attr_accessor :roster
+    attr_reader :school
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort 
+        @roster.each{|key, value| value.sort!}
+    end
+end
